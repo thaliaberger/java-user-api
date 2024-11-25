20f3cb9e3b80f46386fb9689d2edf43ebf0bf518
Thalía Berger <thaliaberger@hotmail.com>
2024-11-25 15:53:38 -0300

    add updateUser

--- src/main/java/br/com/criandoapi/projeto/controller/UserController.java
+++ src/main/java/br/com/criandoapi/projeto/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,12 @@ public class UserController {
     public User createUser(@RequestBody User user) {
         User newUser = dao.save(user);
         return newUser;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        User updatedUser = dao.save(user);
+        return updatedUser;
     }
 
 }
