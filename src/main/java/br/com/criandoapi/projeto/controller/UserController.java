7e718349851622ad9b513d18c5a994edd5c82e2d
Thalía Berger <thaliaberger@hotmail.com>
2024-11-25 15:52:44 -0300

    add createUser

--- src/main/java/br/com/criandoapi/projeto/controller/UserController.java
+++ src/main/java/br/com/criandoapi/projeto/controller/UserController.java
@@ -4,19 +4,30 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.criandoapi.projeto.DAO.IUser;
 import br.com.criandoapi.projeto.model.User;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private IUser dao;
     
-    @GetMapping("/users")
+    @GetMapping
     public List<User> userList() {
         return (List<User>) dao.findAll();
     }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        User newUser = dao.save(user);
+        return newUser;
+    }
+
 }
